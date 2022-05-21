dbbc2f5a9025b8c1ae5ce72d08ccb47eb94e457b
Mubaraque Hussain <mubaraque.nalanda@gmail.com>
2022-05-21 17:10:29 +0530

    Created 10 buttons for the numbers 0 to 9

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import CalculateIcon from '@mui/icons-material/Calculate';
 import styles from './home.module.scss';
 
 export default function Home() {
+  const btnNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   const [output, setOutput] = useState(0);
 
   return (
@@ -13,14 +14,18 @@ export default function Home() {
       </h3>
       <div className={styles.calculator}>
         <div className={styles.output}>{output}</div>
-        <div className={styles.numKeys}>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => setOutput(1)}
-          >
-            1
-          </Button>
+        <div>
+          {btnNumbers.map((num, index) => (
+            <Button
+              className={styles.numKeys}
+              variant="contained"
+              color="primary"
+              onClick={() => setOutput(num)}
+              key={index}
+            >
+              {num}
+            </Button>
+          ))}
         </div>
       </div>
     </div>
