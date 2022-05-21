0439f6f30c35dd9028a8f52995cc0d55b8eec8f5
Mubaraque Hussain <mubaraque.nalanda@gmail.com>
2022-05-21 17:34:48 +0530

    Added a '+' button to allow for the addition of two numbers

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,20 @@ import styles from './home.module.scss';
 export default function Home() {
   const btnNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   const [output, setOutput] = useState(0);
+  const [addition, setAddition] = useState([]);
+
+  const handleInput = input => {
+    var output = input;
+    if (input.length > 1 && input[0] == 0) {
+      output = input.substring(1);
+    }
+    setOutput(output);
+  }
+
+  const handleAddition = input => {
+    setAddition([input]);
+    setOutput(0);
+  }
 
   return (
     <div className={styles.container}>
@@ -18,14 +32,22 @@ export default function Home() {
           {btnNumbers.map((num, index) => (
             <Button
               className={styles.numKeys}
-              variant="contained"
+              variant="outlined"
               color="primary"
-              onClick={() => setOutput(`${output}${num}`)}
+              onClick={() => handleInput(`${output}${num}`)}
               key={index}
             >
               {num}
             </Button>
           ))}
+          <Button
+            className={styles.numKeys}
+            variant="contained"
+            color="primary"
+            onClick={() => handleAddition(parseInt(output))}
+          >
+            +
+          </Button>
         </div>
       </div>
     </div>
